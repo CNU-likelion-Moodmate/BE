57f28a99dd019410928abc934eae017ae739f3ed
daeminkim <kdm0068@gmail.com>
2024-07-25 22:13:44 +0900

    [fix] 임시 메소드들 삭제, QuestController 파일 삭제됨(기능 없음)

--- src/main/java/com/likelion/MoodMate/controller/ChatController.java
+++ src/main/java/com/likelion/MoodMate/controller/ChatController.java
@@ -8,11 +8,9 @@ import com.likelion.MoodMate.service.ChatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/chat")
 public class ChatController {
 
     private final ChatService chatService;
@@ -27,7 +25,7 @@ public class ChatController {
         return chatService.selectModel(request);
     }
 
-    @PostMapping("/message")
+    @PostMapping("/chat")
     public ChatResponse sendMessage(@RequestBody ChatRequest request) {
         return chatService.sendMessage(request);
     }
