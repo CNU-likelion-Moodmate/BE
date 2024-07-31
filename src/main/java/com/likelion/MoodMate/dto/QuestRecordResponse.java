22a6963723dfe8dfe8eebb56cebd36152c7cfaf8
daeminkim <kdm0068@gmail.com>
2024-07-31 23:54:36 +0900

    [fix] loadQuest, completeQuest, deleteQuest, chat에서 fineTuneMessage, conversation history가 없을시 기본값 넣도록 처리

--- src/main/java/com/likelion/MoodMate/dto/QuestRecordResponse.java
+++ src/main/java/com/likelion/MoodMate/dto/QuestRecordResponse.java
@@ -6,11 +6,15 @@ import lombok.Setter;
 @Getter
 @Setter
 public class QuestRecordResponse {
+    private Long questRecordId;
     private String contents;
     private String date;
+    private int rate;
 
-    public QuestRecordResponse(String contents, String date) {
+    public QuestRecordResponse(Long questRecordId, String contents, String date, int rate) {
+        this.questRecordId = questRecordId;
         this.contents = contents;
         this.date = date;
+        this.rate = rate;
     }
 }
