9bf1f4342fdc68a37cef5f0921aa586eb13d0c96
daeminkim <kdm0068@gmail.com>
2024-08-01 21:20:42 +0900

    [fix] post로 변경

--- src/main/java/com/likelion/MoodMate/controller/RecommendQuestController.java
+++ src/main/java/com/likelion/MoodMate/controller/RecommendQuestController.java
@@ -5,6 +5,7 @@ import com.likelion.MoodMate.dto.RecommendQuestResponse;
 import com.likelion.MoodMate.service.QuestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +14,14 @@ import java.util.List;
 @RestController
 public class RecommendQuestController {
 
-    @Autowired
-    private QuestService questService;
+    private final QuestService questService;
 
-    @GetMapping("/recommendQuest")
+    @Autowired
+    public RecommendQuestController(QuestService questService) {
+        this.questService = questService;
+    }
+
+    @PostMapping("/recommendQuest")
     public RecommendQuestResponse recommendQuest(@RequestBody RecommendQuestRequest request) {
         List<String> recommendedQuests = questService.recommendQuests(request.getMood1(), request.getMood2(), request.getActivity());
         return new RecommendQuestResponse(recommendedQuests);
