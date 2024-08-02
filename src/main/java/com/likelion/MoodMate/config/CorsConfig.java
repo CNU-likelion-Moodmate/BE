f020c37e3d5268fcce85e1f29e085c868ddd6585
daeminkim <kdm0068@gmail.com>
2024-08-02 22:15:57 +0900

    [fix] cors 설정 추가

--- src/main/java/com/likelion/MoodMate/config/CorsConfig.java
+++ src/main/java/com/likelion/MoodMate/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://moodmate-fe-e089b358481c.herokuapp.com")
                 .allowedMethods("*")
                 .allowCredentials(true);
     }
